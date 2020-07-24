5fcfb9da150f843e7e78966fa92a9076504541dc
Paithane <apaithane@internal.tavisca.com>
2020-07-24 19:07:06 +0530

    consumer integration test added first commit

---
+++ src/test/java/intg/com/learnkafka/consumer/LibraryEventsConsumerIntegrationTests.java
@@ -0,0 +1,71 @@
+package com.learnkafka.consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.learnkafka.service.LibraryEventsService;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+@EmbeddedKafka(topics = {"lib-test3"}, partitions = 3)
+@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}"
+, "spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}"})
+public class LibraryEventsConsumerIntegrationTests {
+
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    KafkaTemplate<Integer, String> kafkaTemplate;
+
+    @Autowired
+    KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
+
+    @SpyBean
+    LibraryEventsConsumer libraryEventsConsumerSpy;
+
+    @SpyBean
+    LibraryEventsService libraryEventsServiceSpy;
+
+    @BeforeEach
+    void setUp(){
+        for (MessageListenerContainer messageListenerContainer:
+                kafkaListenerEndpointRegistry.getAllListenerContainers()
+             ) {
+            ContainerTestUtils.waitForAssignment(messageListenerContainer, embeddedKafkaBroker.getPartitionsPerTopic());
+        }
+    }
+
+    @Test
+    void publishNewLibraryEvent() throws ExecutionException, InterruptedException, JsonProcessingException {
+
+        String json = "{\"libraryEventType\":\"NEW\",\"libraryEventId\":39,\"book\":{\"bookId\":99,\"bookName\":\"someones BOOK\",\"bookAuthor\":\"Rupesh dugaje\"}}";
+        kafkaTemplate.sendDefault(json).get();
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        countDownLatch.await(3, TimeUnit.SECONDS);
+
+        verify(libraryEventsConsumerSpy, times(1)).onMessage(isA(ConsumerRecord.class));
+        verify(libraryEventsServiceSpy, times(1)).processLibraryEvent(isA(ConsumerRecord.class));
+
+    }
+}
