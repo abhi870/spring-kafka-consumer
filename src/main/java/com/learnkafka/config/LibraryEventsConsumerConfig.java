df9dd5be5f4f09548766f1b4ea41a5374af142d4
Paithane <apaithane@internal.tavisca.com>
2020-07-25 23:29:15 +0530

    comsumer with property and java configs

--- src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -1,12 +1,62 @@
 package com.learnkafka.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.RecoverableDataAccessException;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.retry.RetryPolicy;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableKafka
+@Slf4j
 public class LibraryEventsConsumerConfig {
 
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+
+        log.info("inside config.....................................................");
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory.getIfAvailable());
+        factory.setConcurrency(2);
+        factory.setErrorHandler((thrownException, data) -> {
+            log.info("Exception in consumerConfig is {} and the record is {}", thrownException.getMessage(), data);
+        });
+        factory.setRetryTemplate(retryTemplate());
+        return factory;
+    }
+
+    public RetryTemplate retryTemplate() {
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(100);
+
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(simpleRetryPolicy());
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+        return retryTemplate;
+    }
+
+    public RetryPolicy simpleRetryPolicy() {
+
+        Map<Class<? extends Throwable>, Boolean> exceptions = new HashMap<>();
+        exceptions.put(IllegalArgumentException.class, false);
+        exceptions.put(RecoverableDataAccessException.class, true);
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy(1, exceptions, true);
+        simpleRetryPolicy.setMaxAttempts(7);
+        return simpleRetryPolicy;
+    }
 
 
 }
